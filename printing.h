77223e4c787471e3bc3064b3456d94a52584f49a
Tyler <martin.t@wustl.edu>
2023-10-27 13:28:10 -0500

    printing library that is very likely to change as I decide I do not like it. Provides benefits of overloading observation and type safety from std::cout and std::cerr while maintaining the raceless behavior of printf

---
+++ printing.h
@@ -0,0 +1,48 @@
+#ifndef PRINTING_H
+#define PRINTING_H
+
+#include <sstream>
+
+/*************************************************************************
+
+printing.h
+
+Function that ensures both thread safe printing
+courtesy of std::cout and std::cerr, but allows
+for raceless printing as well without locking
+
+penalties: 
+
+std::cout/cerr - 50 microseconds
+printf - 30 microseconds
+
+benefits: 
+
+Allows for operator overloading
+in the future for printing without encroaching
+on how the printing is handled. Could be replaced
+with C++ 20 idiomatic methods, but I want to keep
+this all C++17 at the absolute worsttime when high criticality mode is entered.
+
+
+Function : cprint(args...)
+
+**************************************************************************/
+
+template <typename Arg, typename... Args>
+static void print(std::ostream& out, Arg&& arg, Args&&... args){   
+    
+    //ostringstream seems to have lowest possible 
+    //overhead
+    std::ostringstream ss;
+    
+    //expander boilerplate
+    ss << std::forward<Arg>(arg);
+    using expander = int[];
+    (void)expander{0, (void(ss << std::forward<Args>(args)), 0)...};
+    
+    //print to whatever we were given
+    out << ss.str();
+}
+
+#endif
