bd56eb7809502f4a71a476975c47404425e8c2ae
Tyler <martin.t@wustl.edu>
2023-11-06 13:09:23 -0600

    updated latch header

--- latch.h
+++ latch.h
@@ -9,6 +9,23 @@
 
 #include "print.h"
 
+/*************************************************************************
+
+latch.h
+
+Custom barrier object. Functions identically to C++20 barriers, but can be 
+stored in shared memory, reinitialized to a new count, and does not require 
+a return type function to be constructed. If provided with a function type
+and a pointer to a function, that function will be executed when the processes 
+exit the barrier. The barrier function exit strategy can either operate on a 
+scheduler mode, where only the scheduling process/thread executes the exit
+function, or on a per-thread basis where each process/thread executes the 
+function as it leaves.
+
+Objects : latch
+
+**************************************************************************/
+
 class latch 
 {
 
