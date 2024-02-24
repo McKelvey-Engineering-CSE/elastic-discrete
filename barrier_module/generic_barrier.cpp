779479d0b815356de619318ef60b644bf7497a04
Tyler-S-M <martin.t@wustl.edu>
2024-02-24 12:34:10 -0600

    Added a real task load via pointless AVX and changed the barrier to use the r_mutex for all locking before value setting for consistency

--- barrier_module/generic_barrier.cpp
+++ barrier_module/generic_barrier.cpp
@@ -1,6 +1,8 @@
 #include "generic_barrier.h"
 #include <iostream>
 
+//#define LOG_STATES
+
 void generic_barrier::init(int in){
     std::lock_guard<std::mutex> lock(mut);
     count = in;
@@ -9,21 +11,45 @@ void generic_barrier::init(int in){
 void generic_barrier::arrive_and_wait()
 {
     //lock mutex
-    std::unique_lock<std::mutex> lock(mut);
+    #ifdef LOG_STATES 
+        std::cout << "waiting at lock" << std::endl;
+    #endif 
 
-    print_module::print(std::cerr, "Barrier Count: ", count, " | ", getppid(), "\n");
+    r_mutex.lock();
+
+    #ifdef LOG_STATES 
+        std::cout << "Lock Grabbed" << std::endl; 
+    #endif
+
+    #ifdef LOG_STATES 
+        print_module::print(std::cerr, "Barrier Count: ", count, " | ", getppid(), "\n");
+    #endif
 
     //check if exit or wait
     if (--count == 0) {
-        lock.unlock();
+
+        #ifdef LOG_STATES 
+            std::cout << "Notifying All Waiting" << std::endl;
+        #endif 
+
+        r_mutex.unlock();
         cv.notify_all();
     } 
     
     else {
-        lock.unlock();
+        r_mutex.unlock();
         while(count != 0){
+
+            #ifdef LOG_STATES 
+                std::cout << "Checking before wait..." << std::endl;
+            #endif
+
             cv.wait(r_mutex);
         }
+
+        #ifdef LOG_STATES 
+            std::cout << "out of wait" << std::endl;
+        #endif
     }
 
     //handle return function
--- target_task/james.cpp
+++ target_task/james.cpp
@@ -1,12 +1,54 @@
 #include "task.h"
+#include <immintrin.h>
+
+#define N 100
+
+int mat1[N][N];
+int mat2[N][N];
+int result[N][N];
 
 int init(int argc, char *argv[])
 {
+
+	std::cout << "Initializing Arrays" << std::endl;
+
+	//give some random values
+	for (int x = 0; x < N; x++){
+		for (int y = 0; y < N; y++){
+			mat1[x][y] = x+y;
+			mat2[x][y] = int(x/2)+int(y/4);
+			result[x][y] = 0;
+		}
+	}
+
 	return 0;       
 }
 
 int run(int argc, char *argv[]){
 	//*(int * ) 0 = 0;
+
+	std::cout << "Executing Matrix Manipulations" << std::endl;
+
+	//Example Vector workload
+	__m256i vec_multi_res = _mm256_setzero_si256();
+	__m256i vec_mat1 = _mm256_setzero_si256();
+	__m256i vec_mat2 = _mm256_setzero_si256();
+
+	int i, j, k;
+	for (i = 0; i < N; i++){
+		for (j = 0; j < N; ++j){
+			//Stores one element in mat1 and use it in all computations needed before proceeding
+			//Stores as vector to increase computations per cycle
+			vec_mat1 = _mm256_set1_epi32(mat1[i][j]);
+
+			for (k = 0; k < N; k += 8){
+				vec_mat2 = _mm256_loadu_si256((__m256i*)&mat2[j][k]);
+				vec_multi_res = _mm256_loadu_si256((__m256i*)&result[i][k]);
+				vec_multi_res = _mm256_add_epi32(vec_multi_res ,_mm256_mullo_epi32(vec_mat1, vec_mat2));
+			}
+		}
+	}
+
 	return 0;
 }
 
