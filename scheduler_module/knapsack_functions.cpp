4ef5fb69da7fd9ec89478a12218f19b2885e48f1
Tyler <martin.t@wustl.edu>
2025-04-24 18:05:43 -0500

    Minor improvement by divergence reduction. Will have to profile to optimize more

--- scheduler_module/knapsack_functions.cpp
+++ scheduler_module/knapsack_functions.cpp
@@ -54,6 +54,15 @@ HOST_DEVICE_CONSTANT double constant_losses[MAXTASKS * MAXMODES];
 
 HOST_DEVICE_GLOBAL void device_do_schedule(int num_tasks, int maxCPU, int NUMGPUS, int* task_table, double* losses, double* final_loss, int* uncooperative_tasks, int* final_solution){
 
+	//shared variables for determining the start and end of 
+	//the indices for uncooperative tasks
+	#ifdef __NVCC__
+
+		__shared__ int start_index;
+		__shared__ int end_index;
+
+	#endif
+
 	//assume 1 block of 1024 threads for now
 	const int pass_count = ceil(((maxCPU + 1) * (NUMGPUS + 1)) / HOST_DEVICE_BLOCK_DIM) + 1;
 
@@ -68,9 +77,52 @@ HOST_DEVICE_GLOBAL void device_do_schedule(int num_tasks, int maxCPU, int NUMGPU
 		int j_end = MAXMODES;
 
 		//check if it is cooperative
-		int desired_state = -1;
-		if (uncooperative_tasks[i - 1])
-			desired_state = uncooperative_tasks[i - 1];
+
+		#ifdef __NVCC__
+		
+		if (uncooperative_tasks[i - 1]){
+
+			if (threadIdx.x < MAXMODES - 1){
+
+				//read one element from the target task modes which is our index
+				int our_element = constant_task_table[(i - 1) * MAXMODES * 3 + threadIdx.x * 3 + 2];
+
+				//read the next element too
+				int next_element = constant_task_table[(i - 1) * MAXMODES * 3 + (threadIdx.x + 1) * 3 + 2];
+
+				//if our element is the uncooperative task target mode, and the next element is not, we have j_end. 
+				if (our_element == uncooperative_tasks[i - 1] && next_element != uncooperative_tasks[i - 1]){
+
+					//store the end index
+					atomicExch(&end_index, threadIdx.x + 1);
+
+				}
+
+				//if our element is not the uncooperative task target mode, and the next element is, we have j_start
+				if (our_element != uncooperative_tasks[i - 1] && next_element == uncooperative_tasks[i - 1]){
+
+					//store the start index
+					atomicExch(&start_index, threadIdx.x);
+
+				}
+
+			}
+
+			__syncthreads();
+
+			//update the start and end indices
+			j_start = start_index;
+			j_end = end_index;
+
+		}
+
+		#else 
+
+			int desired_state = -1;
+			if (uncooperative_tasks[i - 1])
+				desired_state = uncooperative_tasks[i - 1];
+		
+		#endif	
 
 		//for each pass we are supposed to do
 		for (int k = 0; k < pass_count; k++){
@@ -100,15 +152,17 @@ HOST_DEVICE_GLOBAL void device_do_schedule(int num_tasks, int maxCPU, int NUMGPU
 			//for each item in class
 			for (size_t j = j_start; j < j_end; j++) {
 
+				#ifndef __NVCC__
+
+					if (desired_state != -1)
+						if (current_item_real_mode != desired_state)
+							continue;
+				#endif
+
 				//fetch initial suspected resource values
 				int current_item_sms = constant_task_table[(i - 1) * MAXMODES * 3 + j * 3 + 1];
 				int current_item_cores = constant_task_table[(i - 1) * MAXMODES * 3 + j * 3];
 				int current_item_real_mode = constant_task_table[(i - 1) * MAXMODES * 3 + j * 3 + 2];
-
-				//check if the current task is non cooperative
-				if (desired_state != -1)
-					if (current_item_real_mode != desired_state)
-						continue;
 
 				if (current_item_cores == -1 || current_item_sms == -1)
 					continue;
